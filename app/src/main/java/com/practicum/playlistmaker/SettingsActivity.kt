462842e403e749cd1eb4857c2ab7eb6ac3291c66
Boris Moroz <b.frost@yandex.ru>
2024-06-09 13:34:18 +0300

    Спринт 8. Домашнее задание. Версия для ревью 1.0

---
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,25 @@
+package com.practicum.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_settings)
+
+        val buttonBack = findViewById<ImageView>(R.id.button_back)
+
+        buttonBack.setOnClickListener {
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
+
+            //Toast.makeText(this, "Нажали на кнопку \"Назад\"!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
