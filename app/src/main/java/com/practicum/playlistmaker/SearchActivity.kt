bf14b976e5cf2791cd54eb34d58ffe834bb6c61e
Boris Moroz <b.frost@yandex.ru>
2024-07-08 20:56:39 +0300

    Спринт 10. Домашнее задание. Версия для ревью 1.0

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -6,18 +6,61 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.RoundedCorner
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class SearchActivity : AppCompatActivity() {
 
-    private var inputValue : String? = INPUT_VALUE_DEF
+    private var inputValue: String? = INPUT_VALUE_DEF
 
-    private lateinit var inputEditText : EditText
+    private lateinit var inputEditText: EditText
+
+
+    var tracks: ArrayList<Track> = arrayListOf(
+        Track(
+            "Smells Like Teen Spirit",
+            "Nirvana",
+            "5:01",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Billie Jean",
+            "Michael Jackson",
+            "4:35",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Stayin' Alive",
+            "Bee Gees",
+            "4:10",
+            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Whole Lotta Love",
+            "Led Zeppelin",
+            "5:33",
+            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Sweet Child O'Mine",
+            "Guns N' Roses",
+            "5:03",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        )
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +70,22 @@ class SearchActivity : AppCompatActivity() {
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
         inputEditText = findViewById<EditText>(R.id.inputEditText)
 
+        val recyclerView = findViewById<RecyclerView>(R.id.trackList)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        val trackAdapter = trackAdapter(tracks)
+        recyclerView.adapter = trackAdapter
+
         buttonBack.setOnClickListener {
             finish()
-            }
+        }
 
         clearButton.setOnClickListener {
             inputEditText.setText("")
 
-            val inputMethod = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            inputMethod.hideSoftInputFromWindow(inputEditText.windowToken,0)
+            val inputMethod =
+                this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethod.hideSoftInputFromWindow(inputEditText.windowToken, 0)
         }
 
         val inputTextWatcher = object : TextWatcher {
@@ -73,5 +123,48 @@ class SearchActivity : AppCompatActivity() {
     companion object {
         private const val INPUT_VALUE = "INPUT_VALUE"
         private const val INPUT_VALUE_DEF = ""
+    }
+}
+
+data class Track(
+    val trackName: String,
+    val artistName: String, // Имя исполнителя
+    val trackTime: String, // Продолжительность трека
+    val artworkUrl100: String // Ссылка на изображение обложки
+)
+
+class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val trackName: TextView = itemView.findViewById(R.id.trackName)
+    private val artistName: TextView = itemView.findViewById(R.id.artistName)
+    private val image = itemView.findViewById<ImageView>(R.id.image)
+
+    fun bind(track: Track) {
+        trackName.text = track.trackName
+        artistName.text = track.artistName + " • " + track.trackTime
+
+        val radiusInPixels = itemView.context.getResources()
+            .getDimensionPixelSize(R.dimen.tracklst_item_image_radius)
+
+        Glide.with(itemView)
+            .load(track.artworkUrl100)
+            .placeholder(R.drawable.placeholder)
+            .centerCrop()
+            .transform(RoundedCorners(radiusInPixels))
+            .into(image)
+    }
+}
+
+class trackAdapter(private val tracks: List<Track>) : RecyclerView.Adapter<TrackViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
+        return TrackViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        holder.bind(tracks[position])
+    }
+
+    override fun getItemCount(): Int {
+        return tracks.size
     }
 }
